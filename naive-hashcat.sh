0070940d4d2d0c49c9ed182c132d7d9c169e5f64
Lei Shi <me@leishi.io>
2020-04-15 15:02:56 -0700

    Update

--- naive-hashcat.sh
+++ naive-hashcat.sh
@@ -3,6 +3,8 @@
 HASH_FILE="${HASH_FILE:-hashcat-3.6.0/example0.hash}"
 POT_FILE="${POT_FILE:-hashcat.pot}"
 HASH_TYPE="${HASH_TYPE:-0}"
+SESSION="${SESSION:hashcat}"
+DICT="${DICT:dicts/rockyou.txt}"
 # WEIGHT="${WEIGHT:-"medium"}" # light, medium, heavy
 
 # check OSX
@@ -17,9 +19,9 @@ if [ "$(uname)" == 'Darwin' ] ; then
 # check Linux
 elif [ "$(uname)" == 'Linux' ] ; then
 	if [ $(uname -m) == 'x86_64' ]; then
-		HASHCAT="./hashcat-3.6.0/hashcat64.bin"
+		HASHCAT="./hashcat-5.1.0/hashcat64.bin"
 	else
-		HASHCAT="./hashcat-3.6.0/hashcat32.bin"
+		HASHCAT="./hashcat-5.1.0/hashcat32.bin"
 	fi
 # check Windows
 elif [ "$(uname)" == 'MINGW64_NT-10.0' ] ; then
@@ -34,23 +36,23 @@ fi
 # DICTIONARY ATTACK-----------------------------------------------------------------------
 # begin with a _very_ simple and naive dictionary attack. This is blazing fast and 
 # I've seen it crack ~20% of hashes
-"$HASHCAT" -m "$HASH_TYPE" -a 0 "$HASH_FILE" dicts/rockyou.txt --potfile-path "$POT_FILE"
+"$HASHCAT" -m "$HASH_TYPE" -a 0 "$HASH_FILE" --session="$SESSION" "$DICT" --potfile-path "$POT_FILE"
 
 # DICTIONARY ATTACK WITH RULES------------------------------------------------------------
 # now lets move on to a rule based attack, d3ad0ne.rule is a great one to start with
-"$HASHCAT" -m "$HASH_TYPE" -a 0 "$HASH_FILE" dicts/rockyou.txt -r hashcat-3.6.0/rules/d3ad0ne.rule --potfile-path "$POT_FILE"
+"$HASHCAT" -m "$HASH_TYPE" -a 0 "$HASH_FILE" --session="$SESSION" "$DICT" -r hashcat-5.1.0/rules/d3ad0ne.rule --potfile-path "$POT_FILE"
 
 # rockyou is pretty good, and not too slow
-"$HASHCAT" -m "$HASH_TYPE" -a 0 "$HASH_FILE" dicts/rockyou.txt -r hashcat-3.6.0/rules/rockyou-30000.rule --potfile-path "$POT_FILE"
+"$HASHCAT" -m "$HASH_TYPE" -a 0 "$HASH_FILE" --session="$SESSION" "$DICT" -r hashcat-5.1.0/rules/rockyou-30000.rule --potfile-path "$POT_FILE"
 
 
 # MEDIUM
 # dive is a great rule file, but it takes a bit longer to run, so we will run it after d3ad0ne and rockyou
-"$HASHCAT" -m "$HASH_TYPE" -a 0 "$HASH_FILE" dicts/rockyou.txt -r hashcat-3.6.0/rules/dive.rule --potfile-path "$POT_FILE"
+"$HASHCAT" -m "$HASH_TYPE" -a 0 "$HASH_FILE" --session="$SESSION" "$DICT" -r hashcat-5.1.0/rules/dive.rule --potfile-path "$POT_FILE"
 
 # HEAVY
 # MASK ATTACK (BRUTE-FORCE)---------------------------------------------------------------
-"$HASHCAT" -m "$HASH_TYPE" -a 3 "$HASH_FILE" hashcat-3.6.0/masks/rockyou-1-60.hcmask --potfile-path "$POT_FILE"
+"$HASHCAT" -m "$HASH_TYPE" -a 3 "$HASH_FILE" --session="$SESSION" hashcat-5.1.0/masks/rockyou-1-60.hcmask --potfile-path "$POT_FILE"
 
 # COMBINATION ATTACK---------------------------------------------------------------------- 
 # this one can take 12+ hours, don't use it by default
